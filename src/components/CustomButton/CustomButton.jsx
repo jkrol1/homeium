653a8c1f4f79900d81d06c4cbc062a0f9f756206
Jan Król <jk.jankrol@gmail.com>
2020-06-18 23:36:01 +0200

    Add ref prop to the CustomButton component and update styles

--- src/components/CustomButton/CustomButton.jsx
+++ src/components/CustomButton/CustomButton.jsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import './CustomButton.scss';
 
-const CustomButton = ({ content, modifier, ...otherProps }) => (
-    <button {...otherProps} className={`CustomButton ${modifier ? modifier : ''}`}>
-        {content ? content : null}
+const CustomButton = React.forwardRef(({ content, modifier, ...otherProps }, ref) => (
+    <button ref={ref} {...otherProps} className={`CustomButton ${modifier ? modifier : ''}`}>
+        <span className='CustomButton__content'>{content ? content : null}</span>
     </button>
-);
+));
 
 export default CustomButton;
