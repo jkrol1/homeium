85f68d975bf88517b82262c4df7cc99d3f301cfb
Jan Król <jk.jankrol@gmail.com>
2020-06-21 12:26:39 +0200

    Refactor CartIcon components
    Number of items in the Cart is now passed as props.

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -2,11 +2,11 @@ import React from 'react';
 import './CartIcon.scss';
 import { ReactComponent as ShoppingCartSVG } from '../../assets/shopping-cart.svg';
 
-const CartIcon = () => {
+const CartIcon = ({ content }) => {
     return (
         <div className='CartIcon'>
             <ShoppingCartSVG />
-            <span className='CartIcon__number-of-items'>15</span>
+            <span className='CartIcon__number-of-items'>{content}</span>
         </div>
     );
 };
