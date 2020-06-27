3a3682bfce3c81d626e42ea81780c98bcac73995
Jan Król <jk.jankrol@gmail.com>
2020-06-28 00:14:06 +0200

    Add Cart component to Header

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import { selectShowCartPanel } from '../../redux/cart/cartSelectors';
 import Navigation from '../Navigation/Navigation';
+import Cart from '../Cart/Cart';
 import './Header.scss';
 
 const Header = () => {
+
+    const showCartPanel = useSelector(selectShowCartPanel);
+
     return (
         <header className='Header'>
             <div className='fixed-container'>
                 <Navigation />
+                {showCartPanel ? <Cart /> : null}
             </div>
         </header>
     );
