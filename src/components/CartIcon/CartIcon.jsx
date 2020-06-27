debaa7b8c5b19a53e62c12a6d50f5eb78cfd25ec
Jan Król <jk.jankrol@gmail.com>
2020-06-27 23:43:22 +0200

    Enhance extensibility of CartIcon component

--- src/components/CartIcon/CartIcon.jsx
+++ src/components/CartIcon/CartIcon.jsx
@@ -2,9 +2,9 @@ import React from 'react';
 import './CartIcon.scss';
 import { ReactComponent as ShoppingCartSVG } from '../../assets/shopping-cart.svg';
 
-const CartIcon = ({ content }) => {
+const CartIcon = ({ content, ...otherProps }) => {
     return (
-        <div className='CartIcon'>
+        <div className='CartIcon' {...otherProps}>
             <ShoppingCartSVG />
             <span className='CartIcon__number-of-items'>{content}</span>
         </div>
