24a5361b236fac3cc1a34993f013e45098385a0e
Jan Król <jk.jankrol@gmail.com>
2020-06-29 23:41:18 +0200

    Update Cart component
    Cart component will now display provided text message when cart is empty.

--- src/components/Cart/Cart.jsx
+++ src/components/Cart/Cart.jsx
@@ -1,15 +1,18 @@
 import React, { useEffect } from 'react';
 import { useSelector, useDispatch } from 'react-redux';
-import { selectCartItems } from '../../redux/cart/cartSelectors';
+import { selectCartItems, selectNumberOfCartItems } from '../../redux/cart/cartSelectors';
 import { toggleCartPanel } from '../../redux/cart/cartActions';
+import { Link } from 'react-router-dom';
 import CartItem from '../CartItem/CartItem';
 import CustomButton from '../CustomButton/CustomButton';
+import EmptyCartInfo from '../EmptyCartInfo/EmptyCartInfo';
 import './Cart.scss';
 
 const Cart = () => {
 
     const dispatch = useDispatch();
     const cartItems = useSelector(selectCartItems);
+    const numberOfCartItems = useSelector(selectNumberOfCartItems);
 
     useEffect(() => {
 
@@ -27,9 +30,19 @@ const Cart = () => {
             }} className='Cart__close-panel-symbol'>&#10005;</span>
             <h2 className='Cart__heading'>Shopping cart</h2>
             <span className='separation-line'></span>
-            {Object.keys(cartItems).map(cartItem => <CartItem key={cartItem} {...cartItems[cartItem]} />)}
-            <CustomButton content='Go To Checkout' modifier='large-with-border' />
+            {numberOfCartItems > 0 ? (
+                <>
+                    {Object.keys(cartItems).map(cartItem => <CartItem key={cartItem} {...cartItems[cartItem]} />)}
+                    <CustomButton onClick={(e) => {
+                        e.preventDefault();
+                        dispatch(toggleCartPanel());
+                    }}
+                        content={<Link to='/checkout'>Go to checkout</Link>}
+                        modifier='large-with-border' />
+                </>) : <EmptyCartInfo text='There are no items in your shopping cart' />
+            }
         </div>);
+
 };
 
 export default Cart;
