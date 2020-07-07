a42c7e6ad06b87269d04f049ba6588344758a4ff
Jan Król <jk.jankrol@gmail.com>
2020-07-07 20:05:48 +0200

    Update SignInAndSignUp component and its styles
    - Add SignInSignUpChoice component
    - Add CustomButton component
    - Update styles

--- src/pages/SignInAndSignUp/SignInAndSignUp.jsx
+++ src/pages/SignInAndSignUp/SignInAndSignUp.jsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import SignInSignUpChoice from '../../components/SignInSignUpChoice/SignInSignUpChoice';
+import CustomButton from '../../components/CustomButton/CustomButton';
+import { ReactComponent as GoogleSVG } from '../../assets/google.svg';
 import './SignInAndSignUp.scss';
 
 const SignInAndSignUpPage = () => (
-    <div>abcde</div>
+    <main className='SignInAndSignUpPage'>
+        <SignInSignUpChoice />
+        <span className='SignInAndSignUpPage__google-info'>or continue with Google</span>
+        <CustomButton content={<GoogleSVG />} modifier='google' />
+    </main>
 );
 
 export default SignInAndSignUpPage;
