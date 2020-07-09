896aee84b14f2971289d950b2568c9a3a5fceba7
Jan Król <jk.jankrol@gmail.com>
2020-07-09 23:48:48 +0200

    Add user config to the root saga/reducer

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -2,11 +2,13 @@ import { combineReducers } from 'redux';
 import productsReducer from './products/productsReducer';
 import filtersMenuReducer from './filtersMenu/filtersMenuReducer';
 import cartReducer from './cart/cartReducer';
+import userReducer from './user/userReducer';
 
 const rootReducer = combineReducers({
     products: productsReducer,
     filtersMenu: filtersMenuReducer,
-    cart: cartReducer
+    cart: cartReducer,
+    user: userReducer
 });
 
 export default rootReducer;
--- src/redux/rootSaga.js
+++ src/redux/rootSaga.js
@@ -1,7 +1,8 @@
 import { all, call } from 'redux-saga/effects';
 
 import { productsSagas } from './products/productsSagas';
+import { userSagas } from './user/userSagas';
 
 export default function* rootSaga() {
-    yield all([call(productsSagas)]);
+    yield all([call(productsSagas), call(userSagas)]);
 };
