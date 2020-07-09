4603921f547849f7ca7c4af6e812065209500032
Jan Król <jk.jankrol@gmail.com>
2020-07-09 23:49:52 +0200

    Remove comments from store file

--- src/redux/store.js
+++ src/redux/store.js
@@ -8,7 +8,6 @@ const middlewares = [sagaMiddleware];
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 export const store = createStore(rootReducer, composeEnhancers(applyMiddleware(...middlewares)));
-//export const store = createStore(rootReducer, composeEnhancers());
 sagaMiddleware.run(rootSaga);
 
 export default store;
