c8849dc540d0d1c52cdd060c339ec27be186ff4e
Jan Król <jk.jankrol@gmail.com>
2020-07-11 18:45:20 +0200

    Update App component
    - Add useEffect hook to dispatch actions responsible for fetching products
    and checking user session.
    - After SignIn user will be redirected to the '/' route

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
-import { Switch, Route } from 'react-router-dom';
+import React, { useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Switch, Route, Redirect } from 'react-router-dom';
+import { fetchProductsStart } from './redux/products/productsActions';
+import { checkUserSession } from './redux/user/userActions';
+import { selectCurrentUser } from './redux/user/userSelectors';
 import HomePage from './pages/Homepage/Homepage'
 import CategoryPage from './pages/Category/Category'
 import ProductPage from './pages/Product/Product'
@@ -9,11 +12,16 @@ import SignInAndSignUpPage from './pages/SignInAndSignUp/SignInAndSignUp';
 import Header from './components/Header/Header';
 import Footer from './components/Footer/Footer';
 import './App.scss';
-import { fetchProductsStart } from './redux/products/productsActions';
 
 const App = () => {
+
     const dispatch = useDispatch();
-    dispatch(fetchProductsStart());
+    const currentUser = useSelector(selectCurrentUser);
+
+    useEffect(() => {
+        dispatch(fetchProductsStart());
+        dispatch(checkUserSession());
+    }, []);
 
     return (
         <div className='App'>
@@ -22,12 +30,17 @@ const App = () => {
                 <Route exact path='/' component={HomePage} />
                 <Route path='/products/:productId' component={ProductPage} />
                 <Route exact path='/checkout' component={CheckoutPage} />
-                <Route exact path='/signin' component={SignInAndSignUpPage} />
+                <Route exact
+                    path='/signin'
+                    render={() => currentUser
+                        ? <Redirect to='/' />
+                        : <SignInAndSignUpPage />
+                    } />
                 <Route path='/:category' component={CategoryPage} />
             </Switch>
             <Footer />
         </div >
     );
-}
+};
 
 export default App;
