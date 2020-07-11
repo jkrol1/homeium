4c081bfa9e6200f61a85dfa6312b073eae04e013
Jan Król <jk.jankrol@gmail.com>
2020-07-11 18:28:19 +0200

    Implement sign in with google inside SignInAndSignUp component

--- src/pages/SignInAndSignUp/SignInAndSignUp.jsx
+++ src/pages/SignInAndSignUp/SignInAndSignUp.jsx
@@ -1,15 +1,21 @@
 import React from 'react';
+import { useDispatch } from 'react-redux';
+import { googleSignInStart } from '../../redux/user/userActions';
 import SignInSignUpChoice from '../../components/SignInSignUpChoice/SignInSignUpChoice';
 import CustomButton from '../../components/CustomButton/CustomButton';
 import { ReactComponent as GoogleSVG } from '../../assets/google.svg';
 import './SignInAndSignUp.scss';
 
-const SignInAndSignUpPage = () => (
-    <main className='SignInAndSignUpPage'>
-        <SignInSignUpChoice />
-        <span className='SignInAndSignUpPage__google-info'>or continue with Google</span>
-        <CustomButton content={<GoogleSVG />} modifier='google' />
-    </main>
-);
+const SignInAndSignUpPage = () => {
+
+    const dispatch = useDispatch();
+
+    return (
+        <main className='SignInAndSignUpPage'>
+            <SignInSignUpChoice />
+            <span className='SignInAndSignUpPage__google-info'>or continue with Google</span>
+            <CustomButton content={<GoogleSVG />} modifier='google' onClick={() => dispatch(googleSignInStart())} />
+        </main>);
+};
 
 export default SignInAndSignUpPage;
