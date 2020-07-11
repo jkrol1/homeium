ced1a290a9d6a755d622c567fe3c20218586153f
Jan Król <jk.jankrol@gmail.com>
2020-07-11 18:38:08 +0200

    Remove console.log from the code inside ProductRange component

--- src/components/ProductRange/ProductRange.jsx
+++ src/components/ProductRange/ProductRange.jsx
@@ -5,23 +5,17 @@ import { selectProductCategories } from '../../redux/products/productsSelectors'
 import './ProductRange.scss';
 
 const ProductRange = () => {
-    const productCategories = useSelector(selectProductCategories);
-    console.log(productCategories);
 
-    if (!productCategories) {
-        return (
-            <div>Loading...</div>
-        )
-    }
-    else {
-        return (
-            <main className='ProductRange'>
-                {Object.keys(productCategories).map((productCategory, index) =>
-                    <ProductCategory key={`${productCategory}_${index}`} {...productCategories[productCategory]} />)}
-            </main>
-        );
-    }
-}
+    const productCategories = useSelector(selectProductCategories);
+
+    return (
+        <main className='ProductRange'>
+            {Object.keys(productCategories).map((productCategory, index) =>
+                <ProductCategory key={`${productCategory}_${index}`} {...productCategories[productCategory]} />)}
+        </main>
+    );
+};
+
 
 export default ProductRange;
 
