242ed01767a130f243df427e24436a3a06c6d6c0
Jan Król <jk.jankrol@gmail.com>
2020-07-13 16:47:07 +0200

    Add PaginationDot and PaginationDots components
    They'll be used to display number of slides and active slide.

---
+++ src/components/PaginationDot/PaginationDot.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './PaginationDot.scss';
+
+const PaginationDot = ({ modifier }) => (
+    <span className={`PaginationDot ${modifier ? 'PaginationDot--' + modifier : ''}`}></span>
+);
+
+export default PaginationDot;
---
+++ src/components/PaginationDots/PaginationDots.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import PaginationDot from '../PaginationDot/PaginationDot';
+import './PaginationDots.scss';
+
+const PaginationDots = ({ slides, activeSlide }) => (
+    <div className='PaginationDots'>
+        {slides.map((slide, index) => <PaginationDot key={slide + index} modifier={activeSlide === index ? 'active-dot' : null} />)}
+    </div>
+);
+
+export default PaginationDots;
