42de87caae7d2fc0b6bf56506fd0a42285a13276
Jan Król <jk.jankrol@gmail.com>
2020-07-18 23:55:40 +0200

    Add SeparationLine and ClosePanelSymbol components

---
+++ src/components/ClosePanelSymbol/ClosePanelSymbol.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './ClosePanelSymbol.scss';
+
+const ClosePanelSymbol = ({ onClick }) => (
+    <span onClick={onClick} className='ClosePanelSymbol'>&#10005;</span>
+);
+
+export default ClosePanelSymbol;
---
+++ src/components/SeparationLine/SeparationLine.jsx
@@ -0,0 +1,8 @@
+import React from 'react';
+import './SeparationLine.scss';
+
+const SeparationLine = ({ styles }) => (
+    <span className='SeparationLine' style={styles}></span>
+);
+
+export default SeparationLine;
