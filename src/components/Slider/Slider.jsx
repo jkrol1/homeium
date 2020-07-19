727843fcf18f5c372e5b26e1c653876eda15ced6
Jan Król <jk.jankrol@gmail.com>
2020-07-19 23:40:27 +0200

    Update Slider component

--- src/components/Slider/Slider.jsx
+++ src/components/Slider/Slider.jsx
@@ -1,57 +1,115 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect, useRef } from 'react';
+import { useSelector } from 'react-redux';
+import { selectChosenProducts } from '../../redux/products/productsSelectors';
 import Slide from '../Slide/Slide';
-import { ReactComponent as ArrowLeftSVG } from '../../assets/arrow-decrease.svg';
-import { ReactComponent as ArrowRightSVG } from '../../assets/arrow-increase.svg';
+import PaginationDots from '../PaginationDots/PaginationDots';
+import { ReactComponent as ArrowLeftSVG } from '../../assets/arrow-left.svg';
+import { ReactComponent as ArrowRightSVG } from '../../assets/arrow-right.svg';
 import './Slider.scss';
 
 const Slider = () => {
 
-    const images = ["https://images.unsplash.com/photo-1573866926487-a1865558a9cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80", "https://images.unsplash.com/photo-1573866926487-a1865558a9cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80", "https://images.unsplash.com/photo-1573866926487-a1865558a9cf?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1351&q=80"];
+    const slides = useSelector(selectChosenProducts([42, 64, 86]));
+
+    const firstSlide = slides[0].imageUrl
+    const secondSlide = slides[1].imageUrl
+    const lastSlide = slides[slides.length - 1].imageUrl;
+    const autoPlay = 4.5;
+
+    const getWidth = () => window.innerWidth;
+
     const [state, setState] = useState({
-        activeIndex: 0,
-        translate: 0
-    })
+        activeSlide: 0,
+        translate: getWidth(),
+        transition: 0.8,
+        _slides: [lastSlide, firstSlide, secondSlide],
+        isSlideSwitching: false
+    });
 
-    const { activeIndex, translate } = state;
+    const autoPlayRef = useRef();
+    const transitionRef = useRef();
+    const resizeRef = useRef();
 
-    const windowWidth = () => window.innerWidth;
+    const { activeSlide, translate, transition, _slides, isSlideSwitching } = state;
 
-    const nextSlide = () => {
+    useEffect(() => {
+        autoPlayRef.current = nextSlide;
+        transitionRef.current = smoothTransition;
+        resizeRef.current = handleResize;
+    });
 
-        if (activeIndex === images.length - 1) {
-            return setState({
-                ...state,
-                translate: 0,
-                activeIndex: 0
-            });
+    useEffect(() => {
+        const play = () => {
+            autoPlayRef.current();
         };
+
+        const smooth = e => {
+            if (e.target.className.includes('Slider__content')) {
+                transitionRef.current();
+            };
+        };
+
+        const resize = () => {
+            resizeRef.current();
+        };
+
+        const interval = setInterval(play, autoPlay * 1000);
+        const transitionEnd = window.addEventListener('transitionend', smooth);
+        const onResize = window.addEventListener('resize', resize);
+
+        return () => {
+            clearInterval(interval);
+            window.removeEventListener('transitionend', transitionEnd);
+            window.removeEventListener('resize', onResize);
+        }
+    }, []);
+
+    useEffect(() => {
+        if (transition === 0) setState({ ...state, transition: 0.8 })
+    }, [transition]);
+
+    const handleResize = () => {
+        setState({ ...state, translate: getWidth(), transition: 0, isSlideSwitching: false });
+    };
+
+    const smoothTransition = () => {
+        let _slides = [];
+
+        if (activeSlide === slides.length - 1) _slides = [slides[slides.length - 2], lastSlide, firstSlide]
+        else if (activeSlide === 0) _slides = [lastSlide, firstSlide, secondSlide]
+        else _slides = [firstSlide, secondSlide, lastSlide]
+
         setState({
             ...state,
-            activeIndex: activeIndex + 1,
-            translate: windowWidth() * (activeIndex + 1)
+            _slides,
+            transition: 0,
+            translate: getWidth(),
+            isSlideSwitching: false
+        });
+    };
+
+    const nextSlide = () => {
+        if (!isSlideSwitching) setState({
+            ...state,
+            translate: translate + getWidth(),
+            activeSlide: activeSlide === slides.length - 1 ? 0 : activeSlide + 1,
+            isSlideSwitching: true
         });
     };
 
     const prevSlide = () => {
-        if (activeIndex === 0) {
-            return setState({
-                ...state,
-                translate: (images.length - 1) * windowWidth(),
-                activeIndex: images.length - 1
-            });
-        };
-
-        setState({
+        if (!isSlideSwitching) setState({
             ...state,
-            activeIndex: activeIndex - 1,
-            translate: (activeIndex - 1) * windowWidth()
+            translate: 0,
+            activeSlide: activeSlide === 0 ? slides.length - 1 : activeSlide - 1,
+            isSlideSwitching: true
         });
     };
 
     return (
         <section className='Slider'>
-            <div className='Slider__content' style={{ width: `${windowWidth() * images.length}px`, transform: `translateX(-${translate}px)` }}>
-                {images.map((image, index) => <Slide key={image + index} content={image} />)}
+            <div className='Slider__content' style={{ width: `${getWidth() * _slides.length}px`, transform: `translateX(-${translate}px)`, transition: `transform ease-out ${transition}s` }}>
+                {_slides.map((_slide, index) => <Slide key={_slide + index} content={_slide} />)}
             </div>
             <div className='icon-container' onClick={() => prevSlide()}>
                 <ArrowLeftSVG style={{ width: '40%', height: '40%' }} />
@@ -59,6 +117,7 @@ const Slider = () => {
             <div className='icon-container icon-container--arrow-right' onClick={() => nextSlide()}>
                 <ArrowRightSVG style={{ width: '40%', height: '40%' }} />
             </div>
+            <PaginationDots slides={slides} activeSlide={activeSlide} />
         </section>
     );
 };
