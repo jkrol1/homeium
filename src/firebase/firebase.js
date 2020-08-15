5b3c1a785cca2ad394162fc1384a3d0adea3c86d
Jan Król <jk.jankrol@gmail.com>
2020-08-15 15:38:43 +0200

    Refactor firebase functions

--- src/firebase/firebase.js
+++ src/firebase/firebase.js
@@ -4,14 +4,14 @@ import 'firebase/auth';
 //import productData from './productData';
 
 const config = {
-    apiKey: 'AIzaSyCwShblNFQ3IV-2YhEdI3b4B6kE5wOG7oE',
-    authDomain: 'homeium.firebaseapp.com',
-    databaseURL: 'https://homeium.firebaseio.com',
-    projectId: 'homeium',
-    storageBucket: 'homeium.appspot.com',
-    messagingSenderId: '471960765748',
-    appId: '1:471960765748:web:35d66329d4a721e06c1161',
-    measurementId: 'G-EL81L3BV6C'
+  apiKey: 'AIzaSyCwShblNFQ3IV-2YhEdI3b4B6kE5wOG7oE',
+  authDomain: 'homeium.firebaseapp.com',
+  databaseURL: 'https://homeium.firebaseio.com',
+  projectId: 'homeium',
+  storageBucket: 'homeium.appspot.com',
+  messagingSenderId: '471960765748',
+  appId: '1:471960765748:web:35d66329d4a721e06c1161',
+  measurementId: 'G-EL81L3BV6C'
 }
 
 // Initialize firestore
@@ -22,19 +22,19 @@ const db = firebase.firestore();
 
 export const insertItemsToFirestore = (productData) => {
 
-    const batch = db.batch();
-    for (const category in productData) {
-        let docRef = db.collection('products').doc(`${category}`);
-        batch.set(docRef, productData[category]);
-    }
+  const batch = db.batch();
+  for (const category in productData) {
+    let docRef = db.collection('products').doc(`${category}`);
+    batch.set(docRef, productData[category]);
+  }
 
-    batch.commit()
-        .then(function () {
-            console.log('Document successfully written!');
-        })
-        .catch(function (error) {
-            console.error('Error while writing document: ', error);
-        });
+  batch.commit()
+    .then(function () {
+      console.log('Document successfully written!');
+    })
+    .catch(function (error) {
+      console.error('Error while writing document: ', error);
+    });
 };
 
 //insertItemsToFirestore(productData);
@@ -42,48 +42,48 @@ export const insertItemsToFirestore = (productData) => {
 export const firestoreFetch = (path) => (db.collection(path).get());
 
 export const transformProductData = (response) => {
-    const products = {};
-    response.docs.forEach(item => {
-        products[item.id] = item.data();
-    });
-    return products;
+  const products = {};
+  response.docs.forEach(item => {
+    products[item.id] = item.data();
+  });
+  return products;
 };
 
 export const getUserProfile = async (userAuth, additionalData) => {
 
-    // Return if user provided uncorrect credentials
+  // Return if user provided uncorrect credentials
 
-    if (!userAuth) return;
+  if (!userAuth) return;
 
-    const userRef = db.doc(`users/${userAuth.uid}`);
-    const snapshot = await userRef.get();
+  const userRef = db.doc(`users/${userAuth.uid}`);
+  const snapshot = await userRef.get();
 
-    // Create a user profile document if it has not already been created
+  // Create a user profile document if it has not already been created
 
-    if (!snapshot.exists) {
-        const { email } = userAuth;
-        const createdAt = new Date();
-        try {
-            await userRef.set({
-                email,
-                createdAt,
-                ...additionalData
-            });
-        } catch (error) {
-            console.log('Error during user creation', error.message);
-        }
+  if (!snapshot.exists) {
+    const { email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('Error during user creation', error.message);
     }
+  }
 
-    return userRef;
+  return userRef;
 };
 
 export const getCurrentUser = () => {
-    return new Promise((resolve, reject) => {
-        const unsubscribe = auth.onAuthStateChanged(userAuth => {
-            unsubscribe();
-            resolve(userAuth);
-        }, reject);
-    });
+  return new Promise((resolve, reject) => {
+    const unsubscribe = auth.onAuthStateChanged(userAuth => {
+      unsubscribe();
+      resolve(userAuth);
+    }, reject);
+  });
 };
 
 // Add option to sign in with google popup
